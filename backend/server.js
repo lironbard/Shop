aaea1f56762637c119be8a35b7016b0d65a16f9a
lironbard <lironbard@gmail.com>
2021-05-08 16:36:54 +0300

    Connecting to the ATLAS data base - mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./db/db.js";
 import products from "./data/products.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -21,4 +25,4 @@ app.get("/api/products/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`5000 is listening in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, console.log(`5000 is listening in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
