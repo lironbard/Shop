8822bb3daac0b901aa5a0b57563118e3a791d8e1
lironbard <lironbard@gmail.com>
2021-05-08 18:56:36 +0300

    Adding admin and users to the data base

---
+++ backend/data/users.js
@@ -0,0 +1,19 @@
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: xxxx,
+    isAdmin: true,
+  },
+  {
+    name: "Af Echad",
+    email: "af@example.com",
+    password: xxxx,
+    isAdmin: true,
+  },
+  {
+    name: "Simcha Rif",
+    email: "simcha@example.com",
+    password: xxxx,
+  },
+];
