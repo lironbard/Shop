128f85b0a1ea4a5163d55e975fdde49bb3f61c71
lironbard <lironbard@gmail.com>
2021-05-09 16:36:28 +0300

    Adding reducer,details and action to the product

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,4 @@
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from "../constants/productConstants";
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL } from "../constants/productConstants";
 
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
@@ -8,6 +8,20 @@ export const productListReducer = (state = { products: [] }, action) => {
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
     case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productDetailsReducer = (state = { products: { product: { reviews: [] } } }, action) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true, ...state };
+    //the reducer gets the data and put the payload into products
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload };
+    case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
