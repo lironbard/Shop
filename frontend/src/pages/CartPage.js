24680765c7e26bbfd2df096ce15bd6a3a6748cd1
lironbard <lironbard@gmail.com>
2021-05-10 12:08:04 +0300

    Adding cart page functionality

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col, ListGroup, Image, From, Button, Card } from "react-bootstrap";
+import Message from "../components/Message";
+import { addToCart } from "../actions/cartActions";
 
-const CartPage = () => {
+const CartPage = ({ match, location, history }) => {
+  const productId = match.params.id;
+  //---== Getting the number of quantity, right char from the path '?qty=1'---==\\
+  const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+
+  const dispatch = useDispatch();
+
+  //--== putting the item into the cart ==--\\
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  //---== dispatching addToCart only if there is a productId ---==\\
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return <div>Cart</div>;
 };
 
