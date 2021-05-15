508a03df04575beaf0f6d9270db457eed367abaf
lironbard <lironbard@gmail.com>
2021-05-15 21:45:33 +0300

    Creating generateToekn and updating .env,userController files

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
 //--------=== @desc Auth user and get token ===--------\\
@@ -6,16 +7,16 @@ import User from "../models/userModel.js";
 //--------=== @access Public  ===--------\\
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-
+  // --== validating Email ==--\\
   const user = await User.findOne({ email });
-
+  // --== checking password + Email ==--\\
   if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.admin,
-      token: null,
+      token: generateToken(user.id),
     });
   } else {
     res.status(401);
