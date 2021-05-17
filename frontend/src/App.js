04bdf6410d2bf7c908706a27038054f90861390f
lironbard <lironbard@gmail.com>
2021-05-17 22:15:31 +0300

    Adding paymentPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ProfilePage from "./pages/ProfilePage";
 import ShippingPage from "./pages/ShippingPage";
+import PaymentPage from "./pages/PaymentPage";
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Route path="/shipping" component={ShippingPage} />
+          <Route path="/payment" component={PaymentPage} />
           <Route path="/login" component={LoginPage} />
           <Route path="/register" component={RegisterPage} />
           <Route path="/profile" component={ProfilePage} />
