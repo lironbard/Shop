d46c180f53a082a908769b83cc9edaf78bdfb415
lironbard <lironbard@gmail.com>
2021-05-18 12:37:17 +0300

    Updating orderConstants ,orderReducers and orderActions

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL } from "../constants/orderConstants";
+import { ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL } from "../constants/orderConstants";
 
 //--== Order creation action==--\\
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -30,6 +30,39 @@ export const createOrder = (order) => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: ORDER_CREATE_FAIL,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+    });
+  }
+};
+
+//--== Order creation action==--\\
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+  try {
+    //Dispatch request\\
+    dispatch({
+      type: ORDER_DETAILS_REQUEST,
+    });
+    //Getting user info\\
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`/api/order/${id}`, config);
+
+    dispatch({
+      type: ORDER_DETAILS_SUCCESS,
+      payload: data,
+    });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: ORDER_DETAILS_FAIL,
       payload: error.response && error.response.data.message ? error.response.data.message : error.message,
     });
   }
