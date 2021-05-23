e3ad4939b64d0f6d79b3260bdcd6f25419c8856d
lironbard <lironbard@gmail.com>
2021-05-23 21:09:15 +0300

    Adding orders on the profile page

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -68,4 +68,12 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid };
+//--------=== @desc Get orders after login ===--------\\
+//--------=== @route GET api/orders/myorders ===--------\\
+//--------=== @access Private ===--------\\
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await await Order.find({ user: req.user._id });
+  res.json(orders);
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -9,6 +9,9 @@ import {
   ORDER_PAY_REQUEST,
   ORDER_PAY_SUCCESS,
   ORDER_PAY_FAIL,
+  ORDER_LIST_MY_REQUEST,
+  ORDER_LIST_MY_SUCCESS,
+  ORDER_LIST_MY_FAIL,
 } from "../constants/orderConstants";
 
 //--== Order creation action==--\\
@@ -107,6 +110,39 @@ export const payOrder = (orderId, paymentResult) => async (dispatch, getState) =
   } catch (error) {
     dispatch({
       type: ORDER_PAY_FAIL,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+    });
+  }
+};
+
+//--== Order list creation action==--\\
+export const listMyOrders = () => async (dispatch, getState) => {
+  try {
+    //Dispatch request\\
+    dispatch({
+      type: ORDER_LIST_MY_REQUEST,
+    });
+    //Getting user info\\
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`/api/orders/myorders`, config);
+
+    dispatch({
+      type: ORDER_LIST_MY_SUCCESS,
+      payload: data,
+    });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: ORDER_LIST_MY_FAIL,
       payload: error.response && error.response.data.message ? error.response.data.message : error.message,
     });
   }
