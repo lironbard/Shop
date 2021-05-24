714fe6e4c72c5aab72f26e652931368b37a61162
lironbard <lironbard@gmail.com>
2021-05-25 02:17:22 +0300

    Updating productListPage admin create product

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -9,6 +9,9 @@ import {
   PRODUCT_DELETE_FAIL,
   PRODUCT_DELETE_SUCCESS,
   PRODUCT_DELETE_REQUEST,
+  PRODUCT_CREATE_REQUEST,
+  PRODUCT_CREATE_SUCCESS,
+  PRODUCT_CREATE_FAIL,
 } from "../constants/productConstants";
 
 //---== Thunk usage: adding a function inside a function ==---\\
@@ -75,6 +78,37 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_DELETE_FAIL,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+    });
+  }
+};
+
+export const createProduct = () => async (dispatch, getState) => {
+  try {
+    //Dispatch request\\
+    dispatch({
+      type: PRODUCT_CREATE_REQUEST,
+    });
+    //Getting user info\\
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.POST(`/api/products`, {}, config);
+
+    dispatch({
+      type: PRODUCT_CREATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_CREATE_FAIL,
       payload: error.response && error.response.data.message ? error.response.data.message : error.message,
     });
   }
