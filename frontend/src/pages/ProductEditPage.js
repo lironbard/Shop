38045924a92b221eb30addf1db09a701861e3ffe
lironbard <lironbard@gmail.com>
2021-05-25 14:22:33 +0300

    Updating productEditPage with image upload

--- frontend/src/pages/ProductEditPage.js
+++ frontend/src/pages/ProductEditPage.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
@@ -18,6 +19,7 @@ const ProductEditPage = ({ match, history }) => {
   const [category, setCategory] = useState("");
   const [countInStock, setCountInStock] = useState(0);
   const [description, setDescription] = useState("");
+  const [uploading, setUploading] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -45,6 +47,29 @@ const ProductEditPage = ({ match, history }) => {
       }
     }
   }, [dispatch, history, productId, product, successUpdate]);
+
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("image", file);
+    setUploading(true);
+
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      };
+
+      const { data } = await axios.post("/api/upload", formData, config);
+
+      setImage(data);
+      setUploading(false);
+    } catch (error) {
+      console.log(error);
+      setUploading(false);
+    }
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -90,6 +115,8 @@ const ProductEditPage = ({ match, history }) => {
             <Form.Group controlId="image">
               <Form.Label>Image</Form.Label>
               <Form.Control type="text" placeholder="Enter image URL" value={image} onChange={(e) => setImage(e.target.value)}></Form.Control>
+              <Form.File id="image-file" label="Choose File" custom onChange={uploadFileHandler}></Form.File>
+              {uploading && <Loader />}
             </Form.Group>
 
             <Form.Group controlId="brand">
