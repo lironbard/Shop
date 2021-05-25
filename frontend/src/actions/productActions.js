1244abd5d4f753e464524d0ffda56f386641d303
lironbard <lironbard@gmail.com>
2021-05-25 21:53:47 +0300

    Adding products pagination, Paginate component

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -22,12 +22,12 @@ import {
 
 //---== Thunk usage: adding a function inside a function ==---\\
 export const listProducts =
-  (keyword = "") =>
+  (keyword = "", pageNumber = "") =>
   async (dispatch) => {
     try {
       dispatch([{ type: PRODUCT_LIST_REQUEST }]);
       //making the request using axios
-      const { data } = await axios.get(`/api/products?keyword=${keyword}`);
+      const { data } = await axios.get(`/api/products?keyword=${keyword}&pageNumber=${pageNumber}`);
 
       dispatch({
         type: PRODUCT_LIST_SUCCESS,
